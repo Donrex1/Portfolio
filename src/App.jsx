d5c0fa4931ae4fcf9b07c02c366a06242a18ac5e
Donrex1 <drex3795@gmail.com>
2023-06-06 17:44:16 +0100

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+//import React, { useState } from "react"
+//import "./data.css";
 import React from 'react';
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
-import Experience from './components/experience/Expenrience' 
+import Experience from './components/experience/Expenrience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
@@ -13,17 +15,42 @@ const App = () => {
 
   return (
     <div>
-<Header/>
-<Nav/>
-<About/>
-<Experience/>
-<Services/>
-<Portfolio/>
-<Testimonials/>
-<Contact/>
-<Footer/>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 };
 
+//vercel
+
+/*function data() {
+    const [count, setCount] = useState(0);
+
+    function decreaseCount() {
+        if (count > 0) {
+            setCount((prevCount) => prevCount - 1);
+        }
+    }
+}
+
+function increaseCount() {
+    setCount((prevCount) => prevCount + 1);
+}
+return (
+    <div className="data">
+        <div className="counter_container">
+            <button onClick={decreaseCount}>-</button>
+            <span>{count}</span>
+            <button onClick={increaseCount}>+</button>
+        </div>
+    </div>
+);
+*/
 export default App;
